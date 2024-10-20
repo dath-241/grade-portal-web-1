ee1a1bef69b8f0406ae48932fdb5acf4e63d454e
HoangTrungAnh <anh.hoang04k22@hcmut.edu.vn>
2024-10-20 16:59:30 +0700

    change landing page and layout

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,15 @@
 import LandingPage from '../pages/LandingPage/LandingPage';
+import Home from '../pages/Home/Home';
 
 const routes = [
     {
         path: '/',
         component: LandingPage,
-        
+    },
+    {
+        path: '/home',
+        component: Home,
+        layout: true,
     },
 ];
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,10 @@ module.exports = {
     theme: {
         extend: {
             colors: {
-                primary: '#CBF1F5',
-                secondary: '#0688B4',
+                primary: '#112D4E',
+                secondary: '#DBE2EF',
+                third: '#F9F7F7',
+                fourth: '#0688B4',
                 bgColor: '#F7F9FB',
                 textColor: '#0B192C',
             },
