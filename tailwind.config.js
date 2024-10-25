0c928c5baa1f9b447309fa2133a078ef05b0a517
Allen314159 <lammytrangst2014@gmail.com>
2024-10-25 20:56:39 +0700

    Updated sinhvien2 giangvien2

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,17 @@ module.exports = {
     theme: {
         extend: {
             colors: {
+                'themes-black-100': 'var(--themes-black-100)',
+                'themes-black-20': 'var(--themes-black-20)',
+                'themes-black-40': 'var(--themes-black-40)',
+                'themes-black-5': 'var(--themes-black-5)',
                 primary: '#CBF1F5',
                 secondary: '#0688B4',
                 bgColor: '#F7F9FB',
                 textColor: '#0B192C',
+            },
+            fontFamily: {
+                '14-regular': 'var(--14-regular-font-family)',
             },
         },
     },
