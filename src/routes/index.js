6b88abad331e638341fa3e9a451b7bbcda979379
Kim Nhật  Tuấn <tuan.kimnhat@hcmut.edu.vn>
2024-10-26 09:03:47 +0700

    Add files via upload

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,27 @@
-import LandingPage from '../pages/LandingPage/LandingPage';
-
+import Controller from '../pages/AdminPages/Controller';
+import Course1 from '../pages/AdminPages/Course1';
+import Course2 from '../pages/AdminPages/Course2';
+import Course3 from '../pages/AdminPages/Course3';
 const routes = [
     {
         path: '/',
-        component: LandingPage,
-        
+        component: Controller,
+        layout: 'True',
+    },
+    {
+        path: '/Course1',
+        component: Course1,
+        layout: 'True',
+    },
+    {
+        path: '/Course1/course2',
+        component: Course2,
+        layout: 'True',
+    },
+    {
+        path: '/Course1/Course2/Course3',
+        component: Course3,
+        layout: 'True',
     },
 ];
 
