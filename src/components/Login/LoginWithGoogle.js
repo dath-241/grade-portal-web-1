c4b7b5ff28dde94966124e52d36ec854b2af7b70
Bằng <133222169+bawfng04@users.noreply.github.com>
2024-10-28 21:38:22 +0700

    email classification and roles

--- src/components/Login/LoginWithGoogle.js
+++ src/components/Login/LoginWithGoogle.js
@@ -2,15 +2,38 @@ import React from 'react';
 import { GoogleLogin, GoogleOAuthProvider } from '@react-oauth/google';
 import { useNavigate } from 'react-router-dom';
 import { clientID } from './config';
+import { jwtDecode } from 'jwt-decode'; //decode the Google response to get user email
+
+//navigate to
+let navigatePlace = '/home';
+//list accounts
+let teacherEmails = ['nguyendinhbang53@gmail.com', 'abc@gmail.com'];
+let adminEmails = ['nguyendinhbang53az@gmail.com', 'ghi@gmail.com'];
+//
+
+let getRoles = (email) => {
+    if (teacherEmails.includes(email)) return 'teacher';
+    else if (adminEmails.includes(email)) return 'admin';
+    return 'student';
+};
+
+function getUserEmail(res) {
+    let result = jwtDecode(res.credential);
+    if (result) return result.email;
+    return '';
+}
 
 function LoginWithGoogle() {
     const navigate = useNavigate();
     const onSuccess = (response) => {
-        alert('Login with Google success');
-        navigate('/hof');
+        let userEmail = getUserEmail(response);
+        console.log('Login with Google success');
+        console.log('email:', userEmail);
+        console.log('role:', getRoles(userEmail));
+        navigate(navigatePlace);
     };
     const onFailure = (response) => {
-        alert('Login with Google failed: ' + response.error);
+        alert('Login with Google failed: ', response.error);
     };
 
     return (
