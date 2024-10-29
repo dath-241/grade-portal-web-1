5563a0172d2afb067160122bb9d2a369d2bf29fb
HoangTrungAnh <anh.hoang04k22@hcmut.edu.vn>
2024-10-29 17:07:53 +0700

    fix routes header

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -13,8 +13,8 @@ function Header() {
         const pathToIndexMap = {
             '/home': 0,
             '/management': 1,
-            '/student-courses': 2,
-            '/lecturer-courses': 2,
+            '/student-course': 2,
+            '/lecturer-course': 2,
             '/hall-of-fame': 3,
         };
 
@@ -26,35 +26,39 @@ function Header() {
         }
     });
 
+    
+
     return (
-        <div className="fixed left-0 right-0 top-[1rem] z-50 mx-6 flex h-[60px] items-center rounded-2xl bg-opacity-10 bg-gradient-to-r from-[#DBE2EF] to-[#64768C] px-6 py-4">
-            <div className="mr-auto flex items-center">
-                <img src={logo} alt="Logo" className="size-[45px]" />
-                <p className="ml-4 text-xl font-semibold">BK Tra cứu</p>
-            </div>
-            <ul className="headerNav mr-6 flex gap-4 font-medium">
-                <Link to="/home" className="rounded-lg px-4 py-2 text-white hover:bg-primary">
-                    Trang chủ
-                </Link>
+        <div className="header fixed left-0 right-0 top-0 z-50 mx-6  pt-4  bg-bgColor">
+            <div className="z-50 mx-6 flex h-[60px] items-center rounded-2xl bg-opacity-10 bg-gradient-to-r from-[#DBE2EF] to-[#64768C] px-6 py-4">
+                <div className="mr-auto flex items-center">
+                    <img src={logo} alt="Logo" className="size-[45px]" />
+                    <p className="ml-4 text-xl font-semibold">BK Tra cứu</p>
+                </div>
+                <ul className="headerNav mr-6 flex gap-4 font-medium">
+                    <Link to="/home" className="rounded-lg px-4 py-2 text-white hover:bg-primary">
+                        Trang chủ
+                    </Link>
 
-                <Link to="/management" className="rounded-lg px-4 py-2 text-white hover:bg-primary">
-                    Bảng điều khiển
-                </Link>
+                    <Link to="/management" className="rounded-lg px-4 py-2 text-white hover:bg-primary">
+                        Bảng điều khiển
+                    </Link>
 
-                <Link to="/lecturer-courses" className="rounded-lg px-4 py-2 text-white hover:bg-primary">
-                    Khóa học của tôi
-                </Link>
+                    <Link to="/student-courses" className="rounded-lg px-4 py-2 text-white hover:bg-primary">
+                        Khóa học của tôi
+                    </Link>
 
-                <Link to="/hall-of-fame" className="rounded-lg px-4 py-2 text-white hover:bg-primary">
-                    Vinh danh
-                </Link>
-            </ul>
-            <div className="flex items-center">
-                <img
-                    src="https://c4.wallpaperflare.com/wallpaper/821/698/393/anime-naruto-akatsuki-naruto-deidara-naruto-wallpaper-preview.jpg"
-                    alt="avatar"
-                    className="size-[45px] rounded-full object-cover"
-                />
+                    <Link to="/hall-of-fame" className="rounded-lg px-4 py-2 text-white hover:bg-primary">
+                        Vinh danh
+                    </Link>
+                </ul>
+                <div className="flex items-center">
+                    <img
+                        src="https://c4.wallpaperflare.com/wallpaper/821/698/393/anime-naruto-akatsuki-naruto-deidara-naruto-wallpaper-preview.jpg"
+                        alt="avatar"
+                        className="size-[45px] rounded-full object-cover"
+                    />
+                </div>
             </div>
         </div>
     );
@@ -70,7 +74,7 @@ function Footer() {
 
 function Layout({ children }) {
     return (
-        <div className="flex min-h-screen flex-col pt-[5rem] bg-bgColor">
+        <div className="flex min-h-screen flex-col bg-bgColor pt-[5rem]">
             <Header />
             <main className="mx-6 flex-grow py-6">{children}</main>
             <Footer />
