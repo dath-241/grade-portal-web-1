bd295231541dba5fb75776e04f02981e412b35c8
KHOA <khoa47245@gmail.com>
2024-11-02 21:18:28 +0700

    feat: #22 - Add feature call api for student-courses page

--- src/pages/StudentPages/components/course.component.jsx
+++ src/pages/StudentPages/components/course.component.jsx
@@ -1,7 +1,7 @@
-function CourseItem({img,courseName,teacher, semester, group}){
+function CourseItem({id,courseName,teacher, semester, group}){
     return (
       <div className="bg-white  h-[160px] flex p-[10px] rounded-[20px] my-[10px] items-center shadow-lg hover:bg-[#F1F1F1]" >
-         <img src={img} alt="" className="w-[190px] h-[120px] ml-[20px]"/>
+         <img src={`https://avatar.iran.liara.run/public/${id}`} alt="" className="w-[190px] h-[120px] ml-[20px]"/>
          <div className="py-[10px] pl-[50px]">
           <div className="text-black text-[25px] font-light  ">
            {courseName}
